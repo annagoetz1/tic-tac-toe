ab02ecbcefee87f4e6037196778b1c4763bf903e
annagoetz1 <annalgoetz1@gmail.com>
2024-06-07 11:45:24 -0400

    Add function to print to console for player

--- tic.js
+++ tic.js
@@ -5,3 +5,11 @@ function Gameboard () {
       [' ', ' ', ' ']
 };
 
+function printBoard () {
+  console.clear();
+  console.log('  0 1 2');
+  board.forEach((row, index) => {
+    console.log(`${index} ${row.join('|')}`);
+    if (index < 2) console.log('  -----');
+  });
+};

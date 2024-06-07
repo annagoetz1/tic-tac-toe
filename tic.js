7d5e9558065a211aa416ec09fb761d469f2a0956
annagoetz1 <annalgoetz1@gmail.com>
2024-06-07 12:02:24 -0400

    Add logic for win pattern checks

--- tic.js
+++ tic.js
@@ -21,3 +21,19 @@ function makeMove  (row, col, player)  {
   }
   return false;
 };
+
+
+function checkWin (player) {
+  const winPatterns = [
+    [board[0][0], board[0][1], board[0][2]],
+    [board[1][0], board[1][1], board[1][2]],
+    [board[2][0], board[2][1], board[2][2]],
+    [board[0][0], board[1][0], board[2][0]],
+    [board[0][1], board[1][1], board[2][1]],
+    [board[0][2], board[1][2], board[2][2]],
+    [board[0][0], board[1][1], board[2][2]],
+    [board[0][2], board[1][1], board[2][0]],
+  ];
+
+  return winPatterns.some(pattern => pattern.every(cell => cell === player));
+};
